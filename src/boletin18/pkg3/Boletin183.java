8a5c7ad600cf656e5c56e90f49c02cee5cd4a835
FOLFO <FOLFO@FOLFO-PC>
2016-02-03 11:05:19 +0100

    EJERCICIO

---
+++ src/boletin18/pkg3/Boletin183.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin18.pkg3;
+
+/**
+ *
+ * @author FOLFO
+ */
+public class Boletin183 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Metodos obj = new Metodos();
+        obj.notas();
+        System.out.println("\n");
+        obj.ordenarNA();
+        obj.visNA();
+    }
+    
+}
